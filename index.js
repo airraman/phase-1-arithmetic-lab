95200eaa7e6b872b8673fedea3769fa481a6b7a7
Raman <ramanmcreates@gmail.com>
2022-10-10 12:37:48 -0400

    commit tree 699797c132ef5867d61404769dddcbbd0b41868e

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+const num3 = 24
+const num4 = 10
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+
+let mod =  num3 % num4
+
+let max = Math.max(6, 8, 14, 16, 20);
